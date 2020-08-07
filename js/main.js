67624e39eef93f3bbb898b2355ce882ffcbc97da
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-08-07 19:37:10 +0300

    07.08

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
 
+    $('.photo-slider').slick({
+        
+        prevArrow:'<button type="button" class="slick-prev"><img src="images/left.svg" alt=""></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="images/right.svg" alt=""></button>'
+
+    });
+
 });
